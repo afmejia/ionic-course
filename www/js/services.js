8aec3e6751db103f4c79bfc5a2e095500457e100
Felipe <afmejia23@utp.edu.co>
2017-12-29 12:52:23 +0200

    Playing music from spotify

--- www/js/services.js
+++ www/js/services.js
@@ -23,12 +23,56 @@ angular.module('songhop.services', [])
   return o;
 })
 
-.factory('Recommendations', function($http, SERVER) {
+.factory('Recommendations', function($http, SERVER, $q) {
+  var media;
+
   var o = {
     queue: []
   };
 
-  o.getNextSongs = function() {};
+  o.getNextSongs = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations'
+    }).success(function(data) {
+      // merge data into the queue
+      o.queue = o.queue.concat(data);
+    });
+  };
+
+  // used when switching to favorites tab
+  o.haltAudio = function() {
+    if (media) media.pause();
+  };
+
+  o.playCurrentSong = function() {
+    var defer = $q.defer();
+
+    // play the current song's preview
+    media = new Audio(o.queue[0].preview_url);
+
+    // when song loaded, resolve the promise to let controller know.
+    media.addEventListener("loadeddata", function() {
+      defer.resolve();
+    });
+
+    media.play();
+
+    return defer.promise;
+  };
+
+  o.nextSong = function() {
+    // pop the index 0 off
+    o.queue.shift();
+
+    // end the song
+    o.haltAudio();
+
+    // low on the queue? lets fill it up
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+  };
 
   return o;
 });
